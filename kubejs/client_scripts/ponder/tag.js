bd0bdc88060d836f64325e61ce5e29288435970b
XiaoLaoHus <2576381123@qq.com>
2025-07-25 14:16:19 +0800

    Update tag.js

--- kubejs/client_scripts/ponder/tag.js
+++ kubejs/client_scripts/ponder/tag.js
@@ -4,7 +4,7 @@ onEvent("ponder.tag", (event) => {
     "kubejs:rocket", // 你设定的 PonderTag 的 id, 必须小写
     "beyond_earth:rocket_t3", // 你设定的 PonderTag 的图标
     "Rocket", // 你设定的 PonderTag 的名称
-    "Build a rocket ,and fly to distant space.", // 你设定的 PonderTag 的介绍
+    "Build a rocket, and fly to distant space.", // 你设定的 PonderTag 的介绍
   )
 
 })
